255dd1d64c4edeebfa843560bce4952c3fb97528
TorinYu <yutaoitper@gmail.com>
2014-11-01 13:52:05 -0400

    finish message class

--- src/mr/Message.java
+++ src/mr/Message.java
@@ -3,10 +3,27 @@
  */
 package mr;
 
+import java.util.concurrent.Future;
+
+import mr.Type.JOB_STATUS;
+import mr.Type.MESSAGE_TYPE;
+import mr.Type.TASK_TYPE;
+
 /**
  * @author Nicolas_Yu
  *
  */
 public class Message {
-
+	private MESSAGE_TYPE messageType = null;
+    private TASK_TYPE taskType = null;
+    private Object content = null;
+    private String jobId = null; 
+    private String taskId = null;
+    private JOB_STATUS jobStat = null;
+    private TASK_TYPE taskStat = null;
+    //private String blk_fpath = null;
+    private String hostId = null;
+    private Integer aval_procs = -1;
+    private Future future = null;
+    private String output_path = null;
 }
--- src/mr/Type.java
+++ src/mr/Type.java
@@ -11,4 +11,18 @@ public class Type {
 	public enum TASK_TYPE {
 		Mapper, Reducer;
 	}
+	
+	public enum JOB_STATUS {
+		RUNNING, FINISHED, TERMINATED;
+	}
+	
+	public enum MESSAGE_TYPE {
+		REQ_BLK, REL_BLK, SET_JOB_STATUS, 
+        START_MAPPER, TERMINATE_MAPPER, 
+        HEARTBEAT;
+	}
+	
+	public enum TASK_STATUS {
+		RUNNING, FINISHED, TERMINATED;
+	}
 }
