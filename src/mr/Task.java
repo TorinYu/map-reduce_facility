46bba721a9dca95a347d4ee3bea53b2ad6761c1e
TorinYu <yutaoitper@gmail.com>
2014-11-02 16:02:57 -0500

    finsh the run function of Task

--- src/mr/Task.java
+++ src/mr/Task.java
@@ -3,11 +3,17 @@
  */
 package mr;
 
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.ArrayList;
 
+import dfs.DataNode;
+import dfs.NameNode;
 import mr.Type.TASK_TYPE;
+import mr.io.TextWritable;
+import mr.io.Writable;
 
 /**
  * @author Nicolas_Yu
@@ -18,13 +24,13 @@ public class Task implements Runnable{
 	Class<? extends Mapper> mapper = null;
 	Class<? extends Reducer> reducer = null;
 	
-	String job_id = null;
-	String task_id = null;
-	String host_id = null;
-	String block_id = null;
-	int reduceCount = 0;
-	String input_path = null;
-	String output_path = null;
+	String jobId = null;
+	String taskId = null;
+	String hostId = null;
+	String blockId = null;
+	int reduceNum = 0;
+	String inputPath = null;
+	String outputPath = null;
 	String readFromHost = null;
 	
 	Type.TASK_TYPE type = null;
@@ -32,16 +38,19 @@ public class Task implements Runnable{
 	NameNode nameNode = null;
 	
 	Type.TASK_TYPE taskType = null;
-	Registry hdfs_registry = null;
+	Registry hdfsRegistry = null;
 	
 	
 	public Task(String job_id, String task_id, String host, int port) {
-		this.job_id = job_id;
-		this.task_id = task_id;
+		this.jobId = job_id;
+		this.taskId = task_id;
 		try {
-			hdfs_registry = LocateRegistry.getRegistry(host, port);
-			nameNode = hdfs_registry.lookup("NameNode");
+			hdfsRegistry = LocateRegistry.getRegistry(host, port);
+			nameNode = (NameNode) hdfsRegistry.lookup("NameNode");
 		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NotBoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -56,19 +65,294 @@ public class Task implements Runnable{
 	public void run() {
 		// TODO Auto-generated method stub
 		if (type == TASK_TYPE.Mapper) {
-			Mapper<Object, Object, Object, Object> mapClass = mapper.newInstance();
-			
+			try {
+				Mapper<Object, Object, Object, Object> mapClass = mapper.newInstance();
+				String outputPath = "/tmp" + jobId + "/" + hostId + '/';
+				Context context = new Context(jobId, taskId, reduceNum, outputPath, TASK_TYPE.Mapper);
+				// Waiting for JerrySun's API
+				DataNode dataNode = nameNode.getDataNode(readFromHost);
+				String content = dataNode.fetchBlock(blockId);
+				String[] lines = content.split("\\n");
+				for (int i = 0; i < lines.length; i++) {
+					String line = lines[i];
+					TextWritable key = new TextWritable();
+					TextWritable value = new TextWritable();
+					
+					value.setVal(line);
+					mapClass.map(key, value, context);
+				}
+				context.partionMapContent();
+			} catch (InstantiationException | IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+		} else {   //Reduce Task 
+            Reducer<Object, Object, Object, Object> reducerClass = reducer.newInstance();
+            Context context = new Context(jobId, taskId, reduceNum, outputPath, TASK_TYPE.Reducer);
+            //String partition_id = task_id.split("_r_")[1].trim();
+            String input_dir = "/tmp/"+jobId+'/'+hostId+'/';
+            
+            reducerClass.initialize(input_dir);
+            reducerClass.mergePartition();
+
+            ArrayList<RecordLine> reduceLines = reducerClass.getReduceLines();
+            
+            for (int i = 0; i < reduceLines.size(); i++) {
+            	TextWritable key = (TextWritable) reduceLines.get(i).getKey();
+            	Iterable<Writable> values = (Iterable<Writable>) reduceLines.get(i).getValue();
+            	reducerClass.reduce(key, values, context);
+            }
 		}
 		
 	}
 
 
+	public void setMapperClass(Class<? extends Mapper> mapper) {
+		this.mapper = mapper;
+	}
+	
 	/**
-	 * @param mapper2
+	 * @param 
 	 */
-	public void set_taskTP(TASK_TYPE mapper2) {
+	public void setTaskType(TASK_TYPE type) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public void heartBeat() {
+		
+	}
+
+
+	/**
+	 * @return the blockId
+	 */
+	public String getBlockId() {
+		return blockId;
+	}
+
+
+	/**
+	 * @param blockId the blockId to set
+	 */
+	public void setBlockId(String blockId) {
+		this.blockId = blockId;
+	}
+
+
+	/**
+	 * @return the reduceNum
+	 */
+	public int getReduceNum() {
+		return reduceNum;
+	}
+
+
+	/**
+	 * @param reduceNum the reduceNum to set
+	 */
+	public void setReduceNum(int reduceNum) {
+		this.reduceNum = reduceNum;
+	}
+
+
+	/**
+	 * @return the input_path
+	 */
+	public String getInputPath() {
+		return inputPath;
+	}
+
+
+	/**
+	 * @param input_path the input_path to set
+	 */
+	public void setInputPath(String input_path) {
+		this.inputPath = input_path;
+	}
+
+
+	/**
+	 * @return the output_path
+	 */
+	public String getOutput_path() {
+		return outputPath;
+	}
+
+
+	/**
+	 * @param output_path the output_path to set
+	 */
+	public void setOutput_path(String output_path) {
+		this.outputPath = output_path;
+	}
+
+
+	/**
+	 * @return the readFromHost
+	 */
+	public String getReadFromHost() {
+		return readFromHost;
+	}
+
+
+	/**
+	 * @param readFromHost the readFromHost to set
+	 */
+	public void setReadFromHost(String readFromHost) {
+		this.readFromHost = readFromHost;
+	}
+
+
+	/**
+	 * @return the taskType
+	 */
+	public Type.TASK_TYPE getTaskType() {
+		return taskType;
+	}
+
+
+	/**
+	 * @return the mapper
+	 */
+	public Class<? extends Mapper> getMapper() {
+		return mapper;
+	}
+
+
+	/**
+	 * @param mapper the mapper to set
+	 */
+	public void setMapper(Class<? extends Mapper> mapper) {
+		this.mapper = mapper;
+	}
+
+
+	/**
+	 * @return the reducer
+	 */
+	public Class<? extends Reducer> getReducer() {
+		return reducer;
+	}
+
+
+	/**
+	 * @param reducer the reducer to set
+	 */
+	public void setReducer(Class<? extends Reducer> reducer) {
+		this.reducer = reducer;
+	}
+
+
+	/**
+	 * @return the jobId
+	 */
+	public String getJobId() {
+		return jobId;
+	}
+
+
+	/**
+	 * @param jobId the jobId to set
+	 */
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+
+
+	/**
+	 * @return the taskId
+	 */
+	public String getTaskId() {
+		return taskId;
+	}
+
+
+	/**
+	 * @param taskId the taskId to set
+	 */
+	public void setTaskId(String taskId) {
+		this.taskId = taskId;
+	}
+
+
+	/**
+	 * @return the hostId
+	 */
+	public String getHostId() {
+		return hostId;
+	}
+
+
+	/**
+	 * @param hostId the hostId to set
+	 */
+	public void setHostId(String hostId) {
+		this.hostId = hostId;
+	}
+
+
+	/**
+	 * @return the outputPath
+	 */
+	public String getOutputPath() {
+		return outputPath;
+	}
+
+
+	/**
+	 * @param outputPath the outputPath to set
+	 */
+	public void setOutputPath(String outputPath) {
+		this.outputPath = outputPath;
+	}
+
+
+	/**
+	 * @return the type
+	 */
+	public Type.TASK_TYPE getType() {
+		return type;
+	}
+
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(Type.TASK_TYPE type) {
+		this.type = type;
+	}
+
+
+	/**
+	 * @return the nameNode
+	 */
+	public NameNode getNameNode() {
+		return nameNode;
+	}
+
+
+	/**
+	 * @param nameNode the nameNode to set
+	 */
+	public void setNameNode(NameNode nameNode) {
+		this.nameNode = nameNode;
+	}
+
+
+	/**
+	 * @return the hdfsRegistry
+	 */
+	public Registry getHdfsRegistry() {
+		return hdfsRegistry;
+	}
+
+
+	/**
+	 * @param hdfsRegistry the hdfsRegistry to set
+	 */
+	public void setHdfsRegistry(Registry hdfsRegistry) {
+		this.hdfsRegistry = hdfsRegistry;
 	}
 	
 }
