520323687ed525e33b33ae91bcdad7245c7a5a68
JerrySun363 <sunchen363@gmail.com>
2014-11-15 17:20:19 -0500

    add status and terminater

---
+++ src/dfs/Terminater.java
@@ -0,0 +1,24 @@
+package dfs;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Terminater {
+	public static void main(String args[]) {
+		String registryHost = args[0];
+		int registryPort = Integer.parseInt(args[1]);
+		try {
+			Registry registry = LocateRegistry.getRegistry(registryHost,
+					registryPort);
+			NameNode node = (NameNode) registry.lookup("namenode");
+			node.terminate();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
